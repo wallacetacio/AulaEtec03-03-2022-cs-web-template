029b766e5ad57e0272a8f4df9429bdd1de1a3d8b
wallacetacio <wallacett.dev@gmail.com>
2022-03-03 22:27:44 -0300

    Adiciona conteudo da aula de programação web ETEC 03/03/2022

--- Program.cs
+++ Program.cs
@@ -32,9 +32,63 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 // Endpoints da API
-app.MapGet("/api/tarefas", ([FromServices] tarefasContext _db) =>
+app.MapGet("/api/tarefas", (
+    [FromServices] tarefasContext _db
+    ) =>
 {
     return Results.Ok(_db.Tarefa.ToList<Tarefa>());
 });
 
+//busca por id 
+app.MapGet("/api/tarefas/{id}", (
+    [FromServices] tarefasContext _db,
+    [FromRoute] int id
+ ) =>
+{   
+    //busca pelo id que está na rota
+    var tarefa = _db.Tarefa.Find(id);
+
+    //encontrou?
+    if (tarefa == null)
+    {
+        //não encontrou
+        //retornar 404
+        return Results.NotFound();
+    }
+
+    //encontrou
+    //retornar 200 e o0s dados da tarefa
+    return Results.Ok(tarefa);
+});
+
+//Incluir nova tarefa
+app.MapPost("/api/tarefas", (
+    [FromServices] tarefasContext _db,
+   [FromBody] Tarefa novaTarefa
+) =>{
+    if(String.IsNullOrEmpty(novaTarefa.Descricao) )
+    {
+        return Results.BadRequest(new {mensagem = "Informe uma descrição!"});
+    }
+
+    if(novaTarefa.Concluida)
+    {
+        return Results.BadRequest(new {mensagem = "Cadastre a tarefa como pendente."});
+    }
+
+    var tarefa = new Tarefa{
+        Descricao = novaTarefa.Descricao,
+        Concluida = novaTarefa.Concluida,
+    };
+
+    _db.Tarefa.Add(tarefa);
+    _db.SaveChanges();
+
+    string urlTarefaCriada = $"/api/tarefa/{tarefa.Id}";
+
+    return Results.Created(urlTarefaCriada, tarefa);
+
+});
+
 app.Run();
+
